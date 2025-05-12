fe51f9df72ba8605ad24338896011c72f46ba148
mattgh2 <mattwarner744@gmail.com>
2025-05-12 04:31:15 +0000

    ....

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
-require('core.options')
 require('core.plugins')
+require('core.options')
 require('core.mappings')
 require('core.commands')
 
@@ -12,26 +12,49 @@ require('core.configs.dressing')
 require('core.configs.notify')
 require('core.configs.clangd')
 
-require('core.configs.deepwhite')
+-- require('core.configs.deepwhite')
 require('core.configs.lualine')
 require('core.configs.evil_lualine')
 require('core.configs.bufferline')
 require('core.configs.devicons')
 require('core.configs.cmdline')
-require('core.configs.astrotheme')
--- require('core.configs.better_vimui')
-require("mason").setup()
+-- require('core.configs.astrotheme')
+require('core.configs.auto-tag')
 
--- vim.cmd('colorscheme monokai-nightasty')
--- vim.o.background = 'light'
-vim.cmd('colorscheme cs')
+require('mason').setup()
+
+vim.o.background = "dark"
+vim.cmd('colorscheme cascadia')
+
+-- require("neomodern").setup({
+--     theme = "iceclimber",
+--     plugin = {
+--         lualine = {
+--
+--         },
+--     },
+-- })
+
+-- require("neomodern").load()
+
+vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#0f111b", bg = "NONE" }) -- Normal text
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#7aa2f7", bold = true }) -- Matched text
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#bb9af7", bold = true }) -- Fuzzy match
+vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#e0af68", bg = "NONE" }) -- Item kind
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#9aa5ce", bg = "NONE" }) -- Menu text
+
+-- vim.cmd([[highlight Normal guibg=#000087]])
+-- vim.cmd([[highlight Normal guibg=#0f111b]])
+vim.cmd([[highlight NormalFloat guibg=Normal]])
+vim.cmd([[highlight FloatBorder guibg=Normal]])
+vim.cmd([[highlight LineNr guibg=Normal]])
+-- vim.cmd([[highlight LineNr guibg=Normal, guifg=#5fffff]])
+-- vim.cmd([[highlight MatchParen guibg=bold, guifg=#ff0000]])
+vim.cmd([[highlight NonText guifg=Normal guibg=NONE]])
+
+vim.loader.enable() 
 
 
-vim.o.guifont = "Ubuntu Mono derivative Powerline"
 
--- Optimize loading lua modules and startup time
-vim.loader.enable()
 
--- Fixes signcolumn color mismatch
-vim.cmd([[highlight SignColumn guibg=Normal]])
 
